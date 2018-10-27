db66801e7a5c648949e4024715f4c80cbc3651b3
Upasana Mittal <upu1994@gmail.com>
2018-10-27 11:02:19 +0530

    Added jenkins pipelin file

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM python:3.6
+FROM python:2.7	
 COPY .  /app
 WORKDIR /app
 RUN pip install -r requirements.txt
-EXPOSE 5000
+EXPOSE  80
 CMD python app/index.py
--- app/index.py
+++ app/index.py
@@ -2,6 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/hi")
 def hello():
+    print("hurray")
     return "Hello World!"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
